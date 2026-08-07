c8bf24344e8394afc9e92f5037cf9f053ba573b0
Santosh <cse.gandi@gmail.com>
2026-08-07 13:43:45 +0530

    Updated requirements

--- biodata/santosh/data.js
+++ biodata/santosh/data.js
@@ -34,7 +34,7 @@ const DATA = {
     },
     { "label": "Caste", "value": "Velama" },
     { "label": "Address", "value": "Naidu colony, Bobbili, Vizianagaram" },
-    { "label": "Requirements", "value": "Just one - If I like the girl after talking to her in person, I'll proceed.", "highlight": true },
+    { "label": "Requirements", "value": "Just one - If I like the girl after talking to her in person, I'll proceed for lifetime commitment.", "highlight": true },
     { "label": "Contact", "value": ["9010902082", "9963495916"] },
   ],
   "note": {
